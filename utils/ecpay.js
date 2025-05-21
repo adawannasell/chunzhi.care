c831542122e6697932ac86a99e39e59bd38f2306
Ada Shi <adawannaplay@gmail.comm>
2025-05-21 21:52:18 +0800

    修正：綠界 SDK 初始化寫法錯誤

--- utils/ecpay.js
+++ utils/ecpay.js
@@ -1,9 +1,11 @@
+// utils/ecpay.js
+
 const ecpay = require('ecpay_aio_nodejs');
 const options = require('../config/ecpay_options');
 
 function createPaymentHtml(data) {
-  const ecpayClient = new ecpay();
-  return ecpayClient.payment_client.aio_check_out_all(data, options);
+  const ecpayClient = new ecpay.ECPayPayment(options); // ✅ 正確呼叫方式
+  return ecpayClient.payment_client.aio_check_out_all(data);
 }
 
 module.exports = { createPaymentHtml };
