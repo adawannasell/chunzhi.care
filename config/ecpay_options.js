a7f90c4ad7702e16388254d5c26ebabbf66cff55
Ada Shi <adawannaplay@gmail.comm>
2025-05-21 22:12:49 +0800

    修正 SDK 設定檔，加入 OperationMode 與 EncryptType

--- config/ecpay_options.js
+++ config/ecpay_options.js
@@ -1,7 +1,9 @@
+// config/ecpay_options.js
+
 module.exports = {
   MerchantID: process.env.ECPAY_MERCHANT_ID,
   HashKey: process.env.ECPAY_HASH_KEY,
   HashIV: process.env.ECPAY_HASH_IV,
-  OperationMode: 'Test',     // 或 'Production' 正式上線用
+  OperationMode: 'Test',     // ✅ 正確拼寫
   EncryptType: 1
 };
