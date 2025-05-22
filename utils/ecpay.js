3fa09943c06155a6ad63237be57effe6c95a7c9e
Ada Shi <adawannaplay@gmail.comm>
2025-05-22 17:21:00 +0800

    fix: 改用正確 SDK class ECPayPayment 建構金流物件

--- utils/ecpay.js
+++ utils/ecpay.js
@@ -1,9 +1,9 @@
-const ecpay = require('ecpay_aio_nodejs');
+const ECPayPayment = require('ecpay_aio_nodejs');
 const options = require('../config/ecpay_options');
 
 function createPaymentHtml(data) {
-  const ecpayClient = new ecpay(); // ✅ 修正：正確使用 SDK 寫法
-  return ecpayClient.aio_check_out_all(data, options);
+  const ecpayClient = new ECPayPayment(options);
+  return ecpayClient.payment_client.aio_check_out_all(data, options);
 }
 
 module.exports = { createPaymentHtml };
