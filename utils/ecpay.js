c6807788ae5889dbd4a3baa36ee09e8ec47af361
Ada Shi <adawannaplay@gmail.comm>
2025-05-22 18:12:50 +0800

    refactor: 改為使用 XML 模式串接 ECPay 金流

--- utils/ecpay.js
+++ utils/ecpay.js
@@ -1,10 +1,16 @@
 // utils/ecpay.js
 const ECPayPayment = require('ecpay_aio_nodejs');
-const options = require('../config/ecpay_options'); // ✅ 引入你自己寫的 options 設定檔
 
+// ✅ 不再手動傳入 options，直接使用 XML 設定
+const ecpayClient = new ECPayPayment().payment_client();
+
+/**
+ * 根據傳入的訂單資料，產出 ECPay 的付款 HTML 表單
+ * @param {Object} data - 訂單參數物件
+ * @returns {string} HTML 表單字串
+ */
 function createPaymentHtml(data) {
-  const ecpayClient = new ECPayPayment(options); // ✅ 傳入 options
-  return ecpayClient.payment_client.aio_check_out_all(data);
+  return ecpayClient.aio_check_out_all(data);
 }
 
 module.exports = { createPaymentHtml };
