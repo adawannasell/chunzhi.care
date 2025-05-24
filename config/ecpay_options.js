33018e27c430e78c3f7da80aeb40a1b2f667fbd8
Ada Shi <adawannaplay@gmail.comm>
2025-05-24 13:18:42 +0800

    更新首頁視覺設計

--- config/ecpay_options.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  OperationMode: 'Test',           // 'Test' or 'Production'
-  MercProfile: 'Stage_Account',    // ✅ 官方 SDK 必填（你現在就是缺這個）
-  IsProjectContractor: 'N',
-  HashKey: process.env.ECPAY_HASH_KEY,
-  HashIV: process.env.ECPAY_HASH_IV,
-  MerchantID: process.env.ECPAY_MERCHANT_ID,
-  EncryptType: 1
-};
