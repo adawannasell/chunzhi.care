dc8593e814044c48fe02fd3f1c888d3deb1b336e
Ada Shi <adawannaplay@gmail.comm>
2025-05-27 19:24:06 +0800

    fix: 正式加入333 ecpay_options 設定檔到 Git

--- utils/ecpay.js
+++ utils/ecpay.js
@@ -1,6 +1,6 @@
 // utils/ecpay.js
 const ECPay = require('ecpay_aio_nodejs');
-const options = require('../config/ecpay_options.js'); // ✅ 改為 JS 設定
+const options = require('config/ecpay_options.js'); // ✅ 改為 JS 設定
 
 const ecpayClient = new ECPay(options).payment_client();
 
